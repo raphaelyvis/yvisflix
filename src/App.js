95cf0e6e3525acd55ec49d2f6150c8f1ee0b0b19
unknown <raphaelyvis@gmail.com>
2020-07-28 04:23:46 -0300

    Setup inicial do projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './components/Menu';
+import dadosIniciais from './data/dados_iniciais.json'
+import BannerMain from './components/BannerMain'
+import Carousel from './components/Carousel'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{background: "#141414" }} >
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Nessa primeira aula de React finalizaremos a home da AluraFlix! E você vai colocá-la no ar! "
+        + "Usaremos o Create React App para iniciar nosso projeto e styled-components para cuidar da camada de estilo da nossa aplicação,"
+        + "essas duas ferramentas são essenciais do mundo React. Também entenderemos como React se tornou tão popular no mercado"
+        + "de tecnologia."}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />
+
+      <Footer />
+
     </div>
   );
 }
